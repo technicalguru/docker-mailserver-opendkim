65a52db8becc9294e73a879186464066d6685727
ralph <github@ralph-schuster.eu>
2025-10-25 14:50:20 +0000

    Multi-arch container build

--- build.sh
+++ build.sh
@@ -1,1 +1,11 @@
-docker build --progress=plain -t technicalguru/mailserver-dkim:latest .
+docker buildx build \
+     --progress=plain \
+     -t technicalguru/mailserver-opendkim:latest \
+     -t technicalguru/mailserver-opendkim:2.11.0.10 \
+     -t technicalguru/mailserver-opendkim:2.11.0 \
+     -t technicalguru/mailserver-opendkim:2.11 \
+     -t technicalguru/mailserver-opendkim:2 \
+     --push \
+     --platform linux/amd64,linux/arm64 \
+
+#docker build --progress=plain -t technicalguru/mailserver-opendkim:latest .
